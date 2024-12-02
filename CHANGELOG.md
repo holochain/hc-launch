72ddc759ea0e3f4eceb85117a735c47163299969
Matthias <tuttiacc@hotmail.com>
2024-12-02 13:25:36 +0100

    add new zome call signing logic

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/). This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## \[Unreleased\]
+
+### Added
+### Fixed
+### Changed
+- Bump to holochain 0.5.0-dev.7 with new zome call signing logic.
+### Removed
--- crates/holochain_launcher_utils/scripts/src/zomeCallSigning.ts
+++ crates/holochain_launcher_utils/scripts/src/zomeCallSigning.ts
@@ -1,12 +1,12 @@
 import {
   CallZomeRequest,
   CallZomeRequestSigned,
-  CallZomeRequestUnsigned,
   getNonceExpiration,
   randomNonce,
 } from "@holochain/client";
 import { encode } from "@msgpack/msgpack";
 import { invoke } from "@tauri-apps/api";
+import { sha512 } from "js-sha512";
 
 declare global {
   interface Window {
@@ -18,59 +18,33 @@ declare global {
   }
 }
 
-type TauriByteArray = number[]; // Tauri requires a number array instead of a Uint8Array
-
-interface CallZomeRequestUnsignedTauri
-  extends Omit<
-    CallZomeRequestUnsigned,
-    "cap_secret" | "cell_id" | "provenance" | "nonce"
-  > {
-  cell_id: [TauriByteArray, TauriByteArray];
-  provenance: TauriByteArray;
-  nonce: TauriByteArray;
-  expires_at: number;
-}
-
-interface CallZomeRequestSignedTauri
-  extends Omit<
-    CallZomeRequestSigned,
-    "cap_secret" | "cell_id" | "provenance" | "nonce"
-  > {
-  cell_id: [TauriByteArray, TauriByteArray];
-  provenance: TauriByteArray;
-  nonce: TauriByteArray;
-  expires_at: number;
-}
-
 const signZomeCall = async (request: CallZomeRequest) => {
-  const zomeCallUnsigned: CallZomeRequestUnsignedTauri = {
-    provenance: Array.from(request.provenance),
-    cell_id: [Array.from(request.cell_id[0]), Array.from(request.cell_id[1])],
+  if (!request.provenance)
+    return Promise.reject(
+      "Call zome request has provenance field not set. This should be set by the js-client."
+    );
+
+  const zomeCallToSign: CallZomeRequest = {
+    cell_id: request.cell_id,
     zome_name: request.zome_name,
     fn_name: request.fn_name,
-    payload: Array.from(encode(request.payload)),
-    nonce: Array.from(await randomNonce()),
+    payload: encode(request.payload),
+    provenance: request.provenance,
+    nonce: await randomNonce(),
     expires_at: getNonceExpiration(),
   };
 
-  const signedZomeCallTauri: CallZomeRequestSignedTauri = await invoke(
-    "sign_zome_call",
-    { zomeCallUnsigned }
-  );
+  const zomeCallBytes = encode(zomeCallToSign);
+  const bytesHash = sha512.array(zomeCallBytes);
+
+  const signature: number[] = await invoke("sign_zome_call", {
+    bytesHash,
+    pubKey: Array.from(request.provenance),
+  });
 
   const signedZomeCall: CallZomeRequestSigned = {
-    provenance: Uint8Array.from(signedZomeCallTauri.provenance),
-    cap_secret: null,
-    cell_id: [
-      Uint8Array.from(signedZomeCallTauri.cell_id[0]),
-      Uint8Array.from(signedZomeCallTauri.cell_id[1]),
-    ],
-    zome_name: signedZomeCallTauri.zome_name,
-    fn_name: signedZomeCallTauri.fn_name,
-    payload: Uint8Array.from(signedZomeCallTauri.payload),
-    signature: Uint8Array.from(signedZomeCallTauri.signature),
-    expires_at: signedZomeCallTauri.expires_at,
-    nonce: Uint8Array.from(signedZomeCallTauri.nonce),
+    bytes: zomeCallBytes,
+    signature: Uint8Array.from(signature),
   };
 
   return signedZomeCall;
